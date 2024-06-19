2453a5b53f2dc8470343caa4946c8796b95a0601
YihReng <yihrengtan@outlook.com>
2024-06-19 23:16:16 +0800

    footer and showdownjs

--- app.js
+++ app.js
@@ -3,21 +3,45 @@
 // console. log('Button was clicked');
 // });
 
-$(function(){
-  $("#header-placeholder").load("header.html");
-});
+var converter = new showdown.Converter()
+// const text = '# hello, markdown!'
+// const html = converter.makeHtml(text);
 
 function load() {
-    var file = new XMLHttpRequest();
-    file.open("GET", "CocaSinclair.txt", true);
-    file.onreadystatechange = function() {
-      if (file.readyState === 4) {  // Makes sure the document is ready to parse
-        if (file.status === 200) {  // Makes sure it's found the file
-          text = file.responseText;
-          document.getElementById("Section0").innerHTML = text;
-        }
-      }
+  var file = new XMLHttpRequest();
+  file.open("GET", "CocaSinclair.md", true);
+  file.onreadystatechange = function() {
+    if (file.readyState === 4) {  // Makes sure the document is ready to parse
+    if (file.status === 200) {  // Makes sure it's found the file
+      var text = file.responseText;
+      var html = converter.makeHtml(text);
+      $("#section1").html(html);
     }
+    }
+  }
+  file.send();
 }
 
-window.onLoad = load();
+window.onload = load();
+
+$(function(){
+  $("#header-placeholder").load("header.html");
+  $("#footer-placeholder").load("footer.html");
+});
+
+
+
+// function load() {
+//     var file = new XMLHttpRequest();
+//     file.open("GET", "CocaSinclair.txt", true);
+//     file.onreadystatechange = function() {
+//       if (file.readyState === 4) {  // Makes sure the document is ready to parse
+//         if (file.status === 200) {  // Makes sure it's found the file
+//           text = file.responseText;
+//           document.getElementById("Section0").innerHTML = text;
+//         }
+//       }
+//     }
+// }
+
+// window.onLoad = load();
